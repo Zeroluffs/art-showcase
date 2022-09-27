d626a975993f884b1be7ece749a8532356114b54
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-09-27 16:22:27 -0500

    social bar

--- Components/Navigation/NavigationBar.tsx
+++ Components/Navigation/NavigationBar.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import { useRouter, NextRouter } from "next/router";
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faInstagram,
+  faArtstation,
+} from "@fortawesome/free-brands-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import React from "react";
 
 const navigationRoutes = ["Home", "About", "Contact"];
+const socialLinks = [faTwitter, faInstagram, faArtstation];
+
+interface socialProps {
+  icon: IconProp;
+}
 
 export function NavigationBar() {
   const router = useRouter();
-  console.log(typeof router);
   return (
     <nav className="sticky top-0 bg-slate-800 h-28">
       <div className="flex flex-row justify-between mx-4 pt-14 ">
@@ -25,8 +37,12 @@ export function NavigationBar() {
         <div>
           <a className="text-xl text-gray-300">Art Showcase</a>
         </div>
-        <div>
-          <a className="text-sm text-gray-300">d</a>
+        <div className="flex flex-row gap-4">
+          {React.Children.toArray(
+            socialLinks.map((iconName, index) => {
+              return <SocialMediaLinks key={index} icon={iconName} />;
+            })
+          )}
         </div>
       </div>
     </nav>
@@ -48,5 +64,13 @@ function NavigationLink(props: {
         </li>
       </a>
     </Link>
+  );
+}
+
+function SocialMediaLinks({ icon }: socialProps) {
+  return (
+    <a className="text-sm text-gray-300">
+      <FontAwesomeIcon icon={icon} />
+    </a>
   );
 }
---
+++ Components/Navigation/icons/Instagram.js
@@ -0,0 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export function InstagramIcon() {
+  return <FontAwesomeIcon icon="fa-brands fa-instagram" />;
+}
