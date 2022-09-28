96a0ea5c881ca966dcf08e173705fd7c4cca8787
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-09-28 18:11:12 -0500

    opens modal for image

--- Components/Home/ImageCard.tsx
+++ Components/Home/ImageCard.tsx
@@ -1,27 +1,47 @@
 import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
 import midNight from "../../assets/midnight.png";
+import { ImageModal } from "./ImageModal";
 
 const arrayImages = [midNight, midNight, midNight, midNight];
 interface ImageType {
   src: string | StaticImageData;
   className?: string;
+  openingModal: () => void;
 }
 
 export function ImageCard() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  function openingModal() {
+    setIsOpen(true);
+  }
+
   return (
-    <div className="relative grid grid-cols-3 gap-2 pl-24 mt-24 gap gap-y-16">
+    <div className="grid grid-cols-3 gap-2 pl-24 mt-24 gap gap-y-16">
       {arrayImages.map((image, index) => {
-        return <ImageComponent key={index} src={image} />;
+        return (
+          <div key={index}>
+            <ImageComponent src={image} openingModal={openingModal} />
+            {isOpen && <ImageModal src={image} setIsOpen={setIsOpen} />}
+          </div>
+        );
       })}
     </div>
   );
 }
 
-function ImageComponent({ src }: ImageType) {
+function ImageComponent({ src, openingModal }: ImageType) {
   return (
     <div className="w-[500px] h-[500px] relative">
       <Image alt="" src={src} />
-      <div className="absolute inset-0 z-10 flex items-center justify-center text-xl font-semibold text-white duration-300 bg-black opacity-0 hover:opacity-90 hover:bg-opacity-30">
+      <div
+        onClick={() => {
+          openingModal();
+        }}
+        className="absolute inset-0 z-10 flex items-center justify-center text-xl font-semibold text-white duration-300 bg-black opacity-0 hover:opacity-90 hover:bg-opacity-30"
+      >
         Mood
       </div>
     </div>
---
+++ Components/Home/ImageModal.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { useComponentVisible } from "./useComponentVisible";
+import "animate.css";
+
+export function ImageModal({ setIsOpen, src }: any) {
+  const { ref, isComponentVisible } = useComponentVisible(true);
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  setIsOpen(isComponentVisible);
+  return (
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-50 flex items-center justify-center w-screen h-screen bg-black bg-opacity-20 animate__animated animate__fadeIn"
+    >
+      <div className="relative w-10/12 h-5/6">
+        {isComponentVisible && (
+          <Image layout={"fill"} alt="" src={src} objectFit={"contain"} />
+        )}
+      </div>
+    </div>
+  );
+}
