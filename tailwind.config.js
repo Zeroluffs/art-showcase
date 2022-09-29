e8d187b6fa86c5e04d3599be998aebee41efea08
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-09-29 13:42:44 -0500

    added screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
     fontSize: {
       sm: ["20px", "28px"],
       base: ["24px", "32px"],
