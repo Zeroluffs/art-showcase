32178cce8b5537723d25921c0ddb723c3afb37f6
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-10-05 18:15:22 -0500

    asdd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    purge: {
-      content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-      ],
-    },
     extend: {},
     screens: {
       sm: "640px",
